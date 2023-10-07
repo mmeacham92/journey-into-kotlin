c8940d290e34596c88037e08b9edb77bab6c79bb
mmeacham92 <meacham.michael@rocketmail.com>
2023-10-07 18:48:18 -0500

    completed isValidSubsequence problem with Kotlin

--- app/src/main/java/com/example/kotlinnotes/AlgoExpertProblems.kt
+++ app/src/main/java/com/example/kotlinnotes/AlgoExpertProblems.kt
@@ -18,3 +18,23 @@ fun twoNumberSum(array: MutableList<Int>, targetSum: Int): List<Int> {
     }
     return listOf()
 }
+
+
+fun isValidSubsequence(array: List<Int>, sequence: List<Int>): Boolean {
+    // Write your code here.
+    // we will loop through the array, but first
+    // create a position variable for the sequence
+    // position will start at the beginning (index 0) of the sequence
+    // as we loop through the array, we check if the current element in array is the element at
+    //   sequence[position]
+    // if we find a match, we increment position
+    // if position is ever equal to sequence.length, we'll know that the array contains the
+    //  sequence
+
+    var sequenceIndex: Int = 0
+    for (number: Int in array) {
+        if (number === sequence[sequenceIndex]) sequenceIndex += 1
+        if (sequenceIndex === sequence.size) return true
+    }
+    return false
+}
